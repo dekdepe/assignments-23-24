c04c9c7493bd2e706fa7d96917828d550a248c96
Divany <harryndira@gmail.com>
2024-02-25 12:50:17 +0700

    Rilis TP 1

---
+++ assignment1/src/test/java/assignments/assignment1/OrderGeneratorTest.java
@@ -0,0 +1,22 @@
+package assignments.assignment1;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class OrderGeneratorTest {
+    @Test
+    public void testGenerateOrderID1() {
+        assertEquals("HOLY1802202453C3", OrderGenerator.generateOrderID("Holycow!", "18/02/2024", "9928765403"));
+    }
+
+    @Test
+    public void testGenerateBill1() {
+        String solution = "Bill:\n" + //
+                "Order ID: HOLY1802202453C3\n" + //
+                "Tanggal Pemesanan: 18/02/2024\n" + //
+                "Lokasi Pengiriman: S\n" + //
+                "Biaya Ongkos Kirim: Rp 40.000\n" + //
+                "";
+        assertEquals(solution, OrderGenerator.generateBill("HOLY1802202453C3", "s"));
+    }
+}
