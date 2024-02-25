95a21dda1432d76b6db3585b528202650032a5de
Divany <harryndira@gmail.com>
2024-02-25 12:10:29 +0700

    Template done

--- assignment1/src/main/java/assignments/assignment1/OrderGenerator.java
+++ assignment1/src/main/java/assignments/assignment1/OrderGenerator.java
@@ -1,11 +1,64 @@
 package assignments.assignment1;
 
+import java.util.Scanner;
+
 public class OrderGenerator {
-    public static void main(String[] args) {
-        System.out.println("TP");
+    private static final Scanner input = new Scanner(System.in);
+
+    /* 
+    Anda boleh membuat method baru sesuai kebutuhan Anda
+    Namun, Anda tidak boleh menghapus ataupun memodifikasi return type method yang sudah ada.
+    */
+
+    /*
+     * Method  ini untuk menampilkan menu
+     */
+    public static void showMenu(){
+        System.out.println(">>=======================================<<");
+        System.out.println("|| ___                 ___             _ ||");
+        System.err.println("||| . \\ ___  ___  ___ | __>___  ___  _| |||");
+        System.out.println("||| | |/ ._>| . \\/ ._>| _>/ . \\/ . \\/ . |||");
+        System.out.println("|||___/\\___.|  _/\\___.|_| \\___/\\___/\\___|||");
+        System.out.println("||          |_|                          ||");
+        System.out.println(">>=======================================<<");
+        System.out.println();
+        System.out.println("Pilih menu:");
+        System.err.println("1. Generate Order ID");
+        System.out.println("2. Generate Bill");
+        System.out.println("3. Keluar");
     }
 
-    public static String generateOrder() {
+    /*
+     * Method ini digunakan untuk membuat ID
+     * dari nama restoran, tanggal order, dan nomor telepon
+     * 
+     * @return String Order ID dengan format sesuai pada dokumen soal
+     */
+    public static String generateOrderID(String namaRestoran, String tanggalOrder, String noTelepon) {
+        // TODO:Lengkapi method ini sehingga dapat mengenerate Order ID sesuai ketentuan
         return "TP";
     }
+
+
+    /*
+     * Method ini digunakan untuk membuat bill
+     * dari order id dan lokasi
+     * 
+     * @return String Bill dengan format sesuai di bawah:
+     *          Bill:
+     *          Order ID: [Order ID]
+     *          Tanggal Pemesanan: [Tanggal Pemesanan]
+     *          Lokasi Pengiriman: [Kode Lokasi]
+     *          Biaya Ongkos Kirim: [Total Ongkos Kirim]
+     */
+    public static String generateBill(String OrderID, String lokasi){
+        // TODO:Lengkapi method ini sehingga dapat mengenerate Bill sesuai ketentuan
+        return "Bill";
+    }
+
+    public static void main(String[] args) {
+        // TODO: Implementasikan program sesuai ketentuan yang diberikan
+    }
+
+    
 }
--- assignment1/src/test/java/assignments/assignment1/OrderGeneratorTest.java
+++ assignment1/src/test/java/assignments/assignment1/OrderGeneratorTest.java
@@ -5,7 +5,18 @@ import org.junit.jupiter.api.Test;
 
 public class OrderGeneratorTest {
     @Test
-    public void testGenerateOrder() {
-        assertEquals("TP", OrderGenerator.generateOrder());
+    public void testGenerateOrderID1() {
+        assertEquals("HOLY1802202453C3", OrderGenerator.generateOrderID("Holycow!", "18/02/2024", "9928765403"));
+    }
+
+    @Test
+    public void testGenerateBill1() {
+        String solution = "Bill:\n" + //
+                "Order ID: HOLY1802202453C3\n" + //
+                "Tanggal Pemesanan: 18/02/2024\n" + //
+                "Lokasi Pengiriman: S\n" + //
+                "Biaya Ongkos Kirim: Rp 40.000\n" + //
+                "";
+        assertEquals(solution, OrderGenerator.generateBill("HOLY1802202453C3", "s"));
     }
 }
